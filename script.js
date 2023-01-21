c472d667e25f22f2694305fc68a0f11dbf482932
socry <leonardosbo02@gmail.com>
2023-01-21 09:56:19 -0300

    Add coloring function

--- script.js
+++ script.js
@@ -12,20 +12,21 @@ const createGrid = (grid_size) =>{
             cell.style.width = `${width}px`;
             cell.style.height = `${height}px`;
             cell.style.background = "white";
-            cell.style.display = 'inline-block'
-            cell.style.border = '1px solid black'
+            cell.style.display = 'inline-block';
+            cell.style.border = '1px solid black';
+            cell.className = 'cell';
 
             row.appendChild(cell);
         }
-        row.style.cssText = `height: ${height}px`
+        row.style.cssText = `height: ${height}px`;
         grid.appendChild(row);
     }
 }
-createGrid(8)
 
-const btns = document.querySelectorAll('#btns2 button')
+const checkGridSize = () =>{
+    const btns = document.querySelectorAll('#btns2 button')
 
-for (i of btns) {
+for (i of btns){
     i.addEventListener('click', function() {
 
     document.querySelector('#grid').innerHTML = '';
@@ -37,4 +38,40 @@ for (i of btns) {
     }
         
     });
+    }
+}
+
+let color = 'white';
+const getColor = () =>{
+
+const colorButtons = document.querySelectorAll('#btns button')
+    
+    for (i of colorButtons){
+        i.addEventListener('click', function(){
+            btn = this.textContent
+    
+            if (btn == 'Black'){
+                return color = 'black';
+            }
+            else if (btn == 'Eraser'){
+                return color = 'white';
+            }
+            else{
+                return color = 'pink';
+            }
+        })
+    }
+}
+
+
+createGrid(8);
+getColor();
+checkGridSize();
+
+const cells = document.querySelectorAll('.cell');
+
+for (i of cells){
+    i.addEventListener('mouseover', function(){
+        return this.style.background = color;
+    })
 }
